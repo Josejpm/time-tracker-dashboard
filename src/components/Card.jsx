45b00e8ef5c76a65f13e535cb73ebae84e4b8745
Josejpm <josejpm@gmail.com>
2021-11-01 17:41:09 -0300

    json-server installed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ellipsis from '../assets/img/icon-ellipsis.svg'
 
-const Card = () => {
+const Card = ({item}) => {
+
+    console.log(item);
     return ( 
 
         <div className="card">
             <div className="card__header">
-                <p className='card__title' >Work</p>
+                <p className='card__title' >{item.title}</p>
                 <div className="card__ellipsis">
                     <img src={ellipsis} alt="ellipsis"/>
                 </div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,34 @@
-import React,{Fragment} from 'react';
+import axios from 'axios';
+import React,{Fragment, useEffect, useState} from 'react';
 import Card from '../components/Card';
 
 const Dashboard = () => {
+
+    const [data,setData] = useState([])
+
+    useEffect(()=>{
+
+        const apiCall = async()=>{
+            const resp = await axios.get('http://localhost:3004/registries');
+            console.log(data);
+            setData(resp.data);
+        }
+
+        apiCall()
+
+    },[]);
+
+    console.log(data);
     return ( 
         <Fragment>
             <h1>Dashboard</h1>
-            <Card/>
+            {data 
+                ? (data.map(item=>(
+                    <Card key={item.title} item={item} />
+                ))) 
+                : null
+            }
+            <Card data={data} />
         </Fragment>
         
      );
